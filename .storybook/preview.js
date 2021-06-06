0f960fcf947cf9f0972a9a19a15baa36df02a3b5
David Würfel <david.wuerfel@googlemail.com>
2021-06-06 12:01:21 +0200

    feat: add font awesome icons

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+// Add FontAwesome globally (https://fontawesome.com/v5.15/how-to-use/on-the-web/using-with/react)
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
